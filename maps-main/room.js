7516a47995e65f2ac14677422c355a76f2afe19f
voldemord21 <kind58519@gmail.com>
2024-08-10 19:17:00 +0545

    Add files via upload

---
+++ maps-main/room.js
@@ -0,0 +1,28 @@
+var roomJson={
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          85.31307817048912,
+          27.738869650147805
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          85.31911368585355,
+          27.733820419549502
+        ],
+        "type": "Point"
+      }
+    },
+  
+  ]
+}
---
+++ maps-main/room2.js
@@ -0,0 +1,159 @@
+var second={
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            85.31481874973906,
+            27.73567825109727
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            85.31467927486966,
+            27.73631449032918
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            85.31238615702034,
+            27.734204927066486
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            85.3144264902237,
+            27.734156981972617
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            85.3168239762897,
+            27.734540103114824
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            85.31227385268517,
+            27.732542379126343
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            85.3127256337504,
+            27.739575418964264
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            85.31099823555382,
+            27.738023014430425
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            85.31742947191782,
+            27.737975971523497
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            85.32032618581752,
+            27.737928928596304
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            85.32064509009979,
+            27.73418895088909
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            85.31344316838624,
+            27.73171908391744
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            85.31899741797321,
+            27.732989308208644
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            85.31591467657614,
+            27.73832879283026
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
